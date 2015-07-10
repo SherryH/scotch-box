0692c2dc1a7d84bf4172f128f4154e904f7c1407
SherryH <enjoy.wind7rain@gmail.com>
2015-07-10 22:27:47 +1200

    Fresh install of wordpress
    with simon theme and custom child theme following one page website
    tutorial on lynda.com Module2.

---
+++ public/wp-content/themes/lander-ch/front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The custom template for the one-page stype front page. Kicks in automatically
+ *
+ * @package lander-ch
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<div>Hello</div>
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
